44941473ae30202dc451aab1adcb9fbc08033a5a
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-12 17:24:30 +0100

    added new book function

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,23 @@ from models.book_list import booklist
 @app.route('/library')
 def library_main():
     return render_template('index.html',title = "Library", booklist = booklist)
+
+@app.route('/library/<int:index>')
+def specific_book(index):
+    return render_template('specific_book.html', title = 'Book Details', booklist = booklist[index])
+
+@app.route('/library/add_new')
+def new():
+    return render_template('add_new.html')
+
+@app.route('/add_new', methods =['POST'])
+def new_book():
+    book_title = request.form['book_title']
+    author = request.form['author']
+    genre = request.form['genre']
+    isbn = request.form['isbn']
+    summary = request.form['summary']
+    new_book = Book(book_title, author, genre, isbn, summary)
+    booklist.append(new_book)
+    return redirect('/library')
+
--- models/book.py
+++ models/book.py
@@ -1,5 +1,7 @@
 class Book:
-    def __init__(self, book_title, author, genre):
+    def __init__(self, book_title, author, genre, isbn, summary):
         self.book_title = book_title
         self.author = author
         self.genre = genre
+        self.isbn = isbn
+        self.summary = summary
