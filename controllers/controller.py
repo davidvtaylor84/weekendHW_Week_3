165c04b13adae8e04b3335efb3b030b49d43e005
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-12 15:24:02 +0100

    list of titles on page

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,3 +3,6 @@ from app import app
 from models.book import Book
 from models.book_list import booklist
 
+@app.route('/library')
+def library_main():
+    return render_template('index.html',title = "Library", booklist = booklist)
--- models/book_list.py
+++ models/book_list.py
@@ -1,11 +1,14 @@
 
-book1 = ("Do Androids Dream of Electric Sheep", "Philip K. Dick", "Science-Fiction")
-book2 = ("Soul Keeping Company", "Lucie Brock-Broido", "Poetry")
-book3 = ("Wide Sargasso Sea", "Jean Rhys", "Fiction")
-book4 = ("Renegade", "Mark E. Smith", "Biography")
-book5 = ("Blood Meridian", "Cormac McCarthy", "Fiction")
-book6 = ("The Melancholy of Resistance", "László Krasznahorkai", "Fiction")
-book7 = ("Inverted World", "Christopher Priest", "Science-Fiction")
-book8 = ("The Vorhh", "Brian Catling", "Fantasy")
+from .book import Book
+
+
+book1 = Book("Do Androids Dream of Electric Sheep", "Philip K. Dick", "Science-Fiction")
+book2 = Book("Soul Keeping Company", "Lucie Brock-Broido", "Poetry")
+book3 = Book("Wide Sargasso Sea", "Jean Rhys", "Fiction")
+book4 = Book("Renegade", "Mark E. Smith", "Biography")
+book5 = Book("Blood Meridian", "Cormac McCarthy", "Fiction")
+book6 = Book("The Melancholy of Resistance", "László Krasznahorkai", "Fiction")
+book7 = Book("Inverted World", "Christopher Priest", "Science-Fiction")
+book8 = Book("The Vorhh", "Brian Catling", "Fantasy")
 
 booklist = [book1, book2, book3, book4, book5, book6, book7, book8]
