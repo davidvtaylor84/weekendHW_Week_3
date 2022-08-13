ea1c52f3e38a099e051df6e8f6afc655c454d9d3
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-13 15:29:38 +0100

    availability status added

--- models/book.py
+++ models/book.py
@@ -1,9 +1,10 @@
 class Book:
-    def __init__(self, book_title, author, genre, isbn, summary):
+    def __init__(self, book_title, author, genre, isbn, summary, status):
         self.book_title = book_title
         self.author = author
         self.genre = genre
         self.isbn = isbn
         self.summary = summary
+        self.status = status
 
     
--- models/book_list.py
+++ models/book_list.py
@@ -1,22 +1,23 @@
 
+from pickle import TRUE
 from .book import Book
 
 
-book1 = Book("Do Androids Dream of Electric Sheep", "Philip K. Dick", "Science-Fiction", 9781439447093, "Do Androids Dream of Electric Sheep? is a dystopian science fiction novel by American writer Philip K. Dick, first published in 1968. The novel is set in a post-apocalyptic San Francisco, where Earth's life has been greatly damaged by a nuclear global war, leaving most animal species endangered or extinct.")
+book1 = Book("Do Androids Dream of Electric Sheep", "Philip K. Dick", "Science-Fiction", 9781439447093, "Do Androids Dream of Electric Sheep? is a dystopian science fiction novel by American writer Philip K. Dick, first published in 1968. The novel is set in a post-apocalyptic San Francisco, where Earth's life has been greatly damaged by a nuclear global war, leaving most animal species endangered or extinct.", True)
 
-book2 = Book("Soul Keeping Company", "Lucie Brock-Broido", "Poetry", 9789402617030, "Soul Keeping Company' contains a selection of poetry from the pen of Lucy Brock-Broido.")
+book2 = Book("Soul Keeping Company", "Lucie Brock-Broido", "Poetry", 9789402617030, "Soul Keeping Company' contains a selection of poetry from the pen of Lucy Brock-Broido.", True)
 
-book3 = Book("Wide Sargasso Sea", "Jean Rhys", "Fiction", 9789923294680, "Wide Sargasso Sea is a 1966 novel by Dominican-British author Jean Rhys. The novel serves as a postcolonial and feminist prequel to Charlotte Brontë's novel Jane Eyre, describing the background to Mr. Rochester's marriage from the point-of-view of his wife Antoinette Cosway, a Creole heiress.")
+book3 = Book("Wide Sargasso Sea", "Jean Rhys", "Fiction", 9789923294680, "Wide Sargasso Sea is a 1966 novel by Dominican-British author Jean Rhys. The novel serves as a postcolonial and feminist prequel to Charlotte Brontë's novel Jane Eyre, describing the background to Mr. Rochester's marriage from the point-of-view of his wife Antoinette Cosway, a Creole heiress.", False)
 
-book4 = Book("Renegade", "Mark E. Smith", "Biography", 9784180374991, "The only way to appreciate the legendary musician Mark E. Smith is to encounter the man in his own words.")
+book4 = Book("Renegade", "Mark E. Smith", "Biography", 9784180374991, "The only way to appreciate the legendary musician Mark E. Smith is to encounter the man in his own words.", True)
 
-book5 = Book("Blood Meridian", "Cormac McCarthy", "Fiction", 9787206208987, "Blood Meridian or the Evening Redness in the West is a 1985 epic novel by American author Cormac McCarthy, classified under the Western, or sometimes the anti-Western, genre. McCarthy's fifth book, it was published by Random House.")
+book5 = Book("Blood Meridian", "Cormac McCarthy", "Fiction", 9787206208987, "Blood Meridian or the Evening Redness in the West is a 1985 epic novel by American author Cormac McCarthy, classified under the Western, or sometimes the anti-Western, genre. McCarthy's fifth book, it was published by Random House.", False)
 
-book6 = Book("The Melancholy of Resistance", "László Krasznahorkai", "Fiction", 9785970737620, "Hungarian writer László Krasznahorkai. The narrative is set in a restless town where a mysterious circus, which exhibits a whale and nothing else, contributes to an apocalyptic atmosphere.")
+book6 = Book("The Melancholy of Resistance", "László Krasznahorkai", "Fiction", 9785970737620, "Hungarian writer László Krasznahorkai. The narrative is set in a restless town where a mysterious circus, which exhibits a whale and nothing else, contributes to an apocalyptic atmosphere.", False)
 
-book7 = Book("Inverted World", "Christopher Priest", "Science-Fiction", 9785168327602, "Inverted World is a 1974 science fiction novel by British writer Christopher Priest. The novel's basic premise was first used in the short story, The Inverted World, included in New Writings in SF 22, which had different characters and plot. In 2010, the novel was included in the SF Masterworks collection.")
+book7 = Book("Inverted World", "Christopher Priest", "Science-Fiction", 9785168327602, "Inverted World is a 1974 science fiction novel by British writer Christopher Priest. The novel's basic premise was first used in the short story, The Inverted World, included in New Writings in SF 22, which had different characters and plot. In 2010, the novel was included in the SF Masterworks collection.", True)
 
-book8 = Book("The Vorhh", "Brian Catling", "Fantasy", 9787869285564, "The Vorrh is a dark historical fantasy novel by multi-disciplinary artist B. Catling that was first published in November of 2012.")
+book8 = Book("The Vorhh", "Brian Catling", "Fantasy", 9787869285564, "The Vorrh is a dark historical fantasy novel by multi-disciplinary artist B. Catling that was first published in November of 2012.", False)
 
 booklist = [book1, book2, book3, book4, book5, book6, book7, book8]
 
